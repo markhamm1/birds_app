679942b2c149203032e7b98ab30770a740921f5b
Mark <markandrewhamm@gmail.com>
2020-08-05 20:04:56 -0500

    index and show action for Users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,11 @@
 class Api::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render 'index.json.jb'
+  end
+  
+  def show
+    @user = User.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
 end
