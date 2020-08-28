81c69b8c26b0467c9e1758850bcb1a45b7c01f01
Mark <markandrewhamm@gmail.com>
2020-08-28 13:44:52 -0500

    regions, states, and birds controllers and api calls to ebird

--- app/controllers/api/birds_controller.rb
+++ app/controllers/api/birds_controller.rb
@@ -2,7 +2,7 @@ class Api::BirdsController < ApplicationController
   def index
     response = HTTP.headers({
       "X-eBirdApiToken" => "#{Rails.application.credentials.ebird_api[:api_key]}"
-      }).get("https://api.ebird.org/v2/data/obs/US-IL-001/recent")
+      }).get("https://api.ebird.org/v2/data/obs/#{params[:name]}/recent")
     @birds = response.parse
     
     render 'index.json.jb'
